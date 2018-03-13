64a29230fac18d76f0b52b228419c0d7e110c642
Wagner Hitomi <wagner.hitomi@hubchain.io>
2018-03-13 16:24:51 -0300

    fix .travis.yml

---
+++ docker_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+export REPO=$DOCKER_USERNAME/ansible
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
